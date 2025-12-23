dffc380218e8eca8403d0f67be66aacca7571a29
Dewalker <dewakar.agarwal@gmail.com>
2025-12-22 22:20:30 -0500

    add bare JobsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Router } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/mainLayout'
+import JobsPage from './pages/JobsPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />} >
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
     </Route>
   )
 )
