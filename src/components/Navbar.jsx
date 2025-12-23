50e070a0dc293722b89d1e65319b86b0ca8ea20d
Dewalker <dewakar.agarwal@gmail.com>
2025-12-22 22:20:43 -0500

    replace a tag with Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from '../assets/images/logo.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
@@ -6,23 +7,23 @@ const Navbar = () => {
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='flex h-20 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-            {/* <NavLink className='flex flex-shrink-0 items-center mr-4' to='/'> */}
+            <Link className='flex flex-shrink-0 items-center mr-4' to='/'>
               <img className='h-10 w-auto' src={logo} alt='React Jobs' />
               <span className='hidden md:block text-white text-2xl font-bold ml-2'>
                 React Jobs
               </span>
-            {/* </NavLink> */}
+            </Link>
             <div className='md:ml-auto'>
               <div className='flex space-x-2'>
-                {/* <NavLink to='/' className={linkClass}>
+                <Link className='text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' to='/'>
                   Home
-                </NavLink>
-                <NavLink to='/jobs' className={linkClass}>
+                </Link>
+                <Link className='text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' to='/jobs'>
                   Jobs
-                </NavLink>
-                <NavLink to='/add-job' className={linkClass}>
+                </Link>
+                <Link className='text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' to='/add-job'>
                   Add Job
-                </NavLink> */}
+                </Link>
               </div>
             </div>
           </div>
