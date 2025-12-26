684cb82c0974939d19e5763affee0a16f1fed203
Dewalker <dewakar.agarwal@gmail.com>
2025-12-26 17:31:10 -0500

    pass job data via loader

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,29 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-import Spinner from '../components/Spinner';
+import { useParams, useLoaderData} from 'react-router-dom';
 
-const JobPage = () => {
-    const [job, setJob] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const { id } = useParams();
-
-    useEffect(() => {
-        const fetchJob = async () => {
-            try {
-                const response = await fetch(`/api/jobs/${id}`);
-                const data = await response.json();
-                setJob(data);
-            } catch (error) {
-                console.error('Error fetching job data:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchJob();
-    }, []);
-
-  return loading ? <Spinner /> : <h1>{job?.title}</h1>
+const jobLoader = async ({ params }) => {
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data = await res.json();
+    return data;
 }
 
-export default JobPage
+// const JobPage = () => {
+//     const [job, setJob] = useState(null);
+//     const [loading, setLoading] = useState(true);
+//     const { id } = useParams();
+
+//     useEffect(() => {
+//         const fetchJob = async () => {
+//             try {
+//                 const response = await fetch(`/api/jobs/${id}`);
+//                 const data = await response.json();
+//                 setJob(data);
+//             } catch (error) {
+//                 console.error('Error fetching job data:', error);
+//             } finally {
+//                 setLoading(false);
+//             }
+//         };
+
+//         fetchJob();
+//     }, []);
+
+//   return loading ? <Spinner /> : <h1>{job?.title}</h1>
+// }
+
+const JobPage = () => {
+    const { id } = useParams();
+    const job = useLoaderData();
+    return <h1>{job?.title}</h1>
+}
+
+export { JobPage as default, jobLoader };
