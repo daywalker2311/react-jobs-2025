b904a02669b3225fb583147460153de3fd2b787d
Dewalker <dewakar.agarwal@gmail.com>
2025-12-26 18:45:21 -0500

    add job info to show

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,4 +1,6 @@
-import { useParams, useLoaderData} from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
+import { FaLocationDot } from 'react-icons/fa6';
+import { useParams, useLoaderData, Link} from 'react-router-dom';
 
 const jobLoader = async ({ params }) => {
     const res = await fetch(`/api/jobs/${params.id}`);
@@ -33,7 +35,79 @@ const jobLoader = async ({ params }) => {
 const JobPage = () => {
     const { id } = useParams();
     const job = useLoaderData();
-    return <h1>{job?.title}</h1>
+    return (
+        <>
+            <section>
+                <div className='container m-auto py-6 px-6'>
+                    <Link 
+                    className='text-indigo-500 hover:text-indigo-600 flex items-center'
+                    to='/jobs'><FaArrowLeft className='mr-2'/> Back to Jobs</Link>
+                </div>
+            </section>
+            <section className='bg-indogo-50'>
+                <div className='container m-auto py-10 px-6'>
+                    <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+                        <main>
+                            <div className='bg-white p-6 rounded-lg shadow-md text-center md:text-left'>
+                                <div className='text-gray-500 mb-4'>{job.type}</div>
+                                <h1 className='text-3xl font-bold mb-4'>{job?.title}</h1>
+                                <div className='text-gray-500 mb-4 flex align-middle justify-center md:justify-start'>
+                                    <FaLocationDot className='text-lg text-orange-700 mr-2'/>
+                                    <p className='text-orange-700'>{job?.location}</p>
+                                </div>
+                            </div>
+
+                            <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
+                                <h3 className='text-indigo-800 text-lg font-bold mb-6'>
+                                    Job Description
+                                </h3>
+
+                                <p className='mb-4'>{job?.description}</p>
+
+                                <h3 className='text-indigo-800 text-lg font-bold mb-2'>
+                                    Salary
+                                </h3>
+                                <p className='mb-4'>{job?.salary} / Year</p>
+                            </div>
+                        </main>
+
+                        <aside>
+                             <div className='bg-white p-6 rounded-lg shadow-md'>
+                                <h3 className='text-xl font-bold mb-6'>
+                                    Company
+                                </h3>
+
+                                <h2 className='text-2xl'>{job.company.name}</h2>
+
+                                <p className='py-2'>{job.company.description}</p>
+
+                                <hr className='my-4'/>
+
+                                <h3 className='text-xl'>Contact Email:</h3>
+                                <p className='my-2 bg-indigo-100 p-2 font-bold'>{job.company.contactEmail}</p>
+                                
+                                <h3 className='text-xl'>Contact Phone:</h3>
+                                <p className='my-2 bg-indigo-100 p-2 font-bold'>{job.company.contactPhone}</p>
+                             </div>
+
+                             <div className='bg-white p-6 rounded shadow-md mt-6'>
+                                <h3 className='text-xl font-bold mb-6'>Manage Job</h3>
+
+                                <Link 
+                                className='bg-indigo-500 hover:bg-indigo-600 text-white text-center py-2 px-4 rounded-full w-full  focus:outline-none focus:shadow-outline mt-4 block'
+                                to={`/jobs/${id}/edit`}
+                                >Edit Job</Link>
+                                
+                                <button 
+                                className='bg-red-500 hover:bg-red-600 text-white text-center py-2 px-4 rounded-full w-full  focus:outline-none focus:shadow-outline mt-4 block'
+                                >Delete Job</button>
+                             </div>
+                        </aside>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export { JobPage as default, jobLoader };
