7eeaac9e1d1b7b90d17951333d46f7234b07ff1a
Dewalker <dewakar.agarwal@gmail.com>
2025-12-26 16:56:23 -0500

    add JobPage to show individual job related data

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -40,7 +40,7 @@ const JobListing = ({job}) => {
                         <FaMapMarker className='inline text-lg mb-1 mr-1' />
                         {job.location}
                     </div>
-                    <a href={`/job/${job.id}`} className='h-[36px] bg-indigo-600 text-white px-2 py-2 rounded-lg text-center text-sm'> 
+                    <a href={`/jobs/${job.id}`} className='h-[36px] bg-indigo-600 text-white px-2 py-2 rounded-lg text-center text-sm'> 
                         Read More
                     </a>
                 </div>
